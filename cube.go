96a447c873c781e5385817f5c6a03a966f7c1556
yyy <y.w.k.w.1717@gmail.com>
2018-05-04 21:29:20 +0900

    [Add] some rotation method

--- cube.go
+++ cube.go
@@ -1,30 +1,5 @@
-package main
+package cube
 
-import (
-	"fmt"
-	"gonum.org/v1/gonum/mat"
-)
-
-/*
-##########################################
-#          Cube State                    #
-#                                        #
-#          ----------                    #
-#          |U1|U2|U3|                    #
-#          |U4|U5|U6|                    #
-#          |U7|U8|U9|                    #
-# ---------|--------|------------------  #
-# |L1|L2|L3|F1|F2|F3|R1|R2|R3|B1|B2|B3|  #
-# |L4|L5|L6|F4|F5|F6|R4|R5|R6|B4|B5|B6|  #
-# |L7|L8|L9|F7|F8|F9|R7|R8|R9|B7|B8|B9|  #
-# ---------|--------|------------------  #
-#          |D1|D2|D3|                    #
-#          |D4|D5|D6|                    #
-#          |D7|D8|D9|                    #
-#          ----------                    #
-#                                        #
-##########################################
-*/
 const (
 	WHITE = iota
 	ORANGE
@@ -34,47 +9,132 @@ const (
 	YELLOW
 )
 
-type U *mat.Dense
-type L *mat.Dense
-type F *mat.Dense
-type R *mat.Dense
-type B *mat.Dense
-type D *mat.Dense
-
 type Cube struct {
-	u U
-	l L
-	f F
-	r R
-	b B
-	d D
+	U [][]int
+	L [][]int
+	F [][]int
+	R [][]int
+	B [][]int
+	D [][]int
 }
 
 func NewCube() *Cube {
 	cube := new(Cube)
-	cube.u = mat.NewDense(3, 3, makeCubeData(WHITE))
-	cube.l = mat.NewDense(3, 3, makeCubeData(ORANGE))
-	cube.f = mat.NewDense(3, 3, makeCubeData(GREEEN))
-	cube.r = mat.NewDense(3, 3, makeCubeData(RED))
-	cube.b = mat.NewDense(3, 3, makeCubeData(BLUE))
-	cube.d = mat.NewDense(3, 3, makeCubeData(YELLOW))
+	cube.U = makeCubeData(WHITE)
+	cube.L = makeCubeData(ORANGE)
+	cube.F = makeCubeData(GREEEN)
+	cube.R = makeCubeData(RED)
+	cube.B = makeCubeData(BLUE)
+	cube.D = makeCubeData(YELLOW)
 	return cube
 }
 
-func makeCubeData(color int) []float64 {
-	data := make([]float64, 9)
+func makeCubeData(color int) [][]int {
+	data := make([][]int, 3)
 	for i := range data {
-		data[i] = float64(color)
+		data[i] = []int{color, color, color}
 	}
 	return data
 }
 
-func main() {
-	cube := NewCube()
-	fmt.Println(cube.u)
-	fmt.Println(cube.l)
-	fmt.Println(cube.f)
-	fmt.Println(cube.r)
-	fmt.Println(cube.b)
-	fmt.Println(cube.d)
+func (cube *Cube) rRotation() {
+	dup := Copy(cube)
+
+	cube.U[0][2] = dup.F[0][2]
+	cube.U[1][2] = dup.F[1][2]
+	cube.U[2][2] = dup.F[2][2]
+
+	cube.F[0][2] = dup.D[0][2]
+	cube.F[1][2] = dup.D[1][2]
+	cube.F[2][2] = dup.D[2][2]
+
+	cube.D[2][2] = dup.B[0][0]
+	cube.D[1][2] = dup.B[1][0]
+	cube.D[0][2] = dup.B[2][0]
+
+	cube.B[0][0] = dup.U[2][2]
+	cube.B[1][0] = dup.U[1][2]
+	cube.B[2][0] = dup.U[0][2]
+}
+
+func (cube *Cube) rPrimeRotation() {
+	dup := Copy(cube)
+
+	cube.U[0][2] = dup.B[2][0]
+	cube.U[1][2] = dup.B[1][0]
+	cube.U[2][2] = dup.B[0][0]
+
+	cube.F[0][2] = dup.U[0][2]
+	cube.F[1][2] = dup.U[1][2]
+	cube.F[2][2] = dup.U[2][2]
+
+	cube.D[0][2] = dup.F[0][0]
+	cube.D[1][2] = dup.F[1][0]
+	cube.D[2][2] = dup.F[2][0]
+
+	cube.B[0][0] = dup.D[0][2]
+	cube.B[1][0] = dup.D[1][2]
+	cube.B[2][0] = dup.D[2][2]
+}
+
+func (cube *Cube) r2Rotation() {
+	cube.rRotation()
+	cube.rRotation()
+}
+
+func Copy(cube *Cube) *Cube {
+	dup := NewCube()
+
+	deepCopyTwoDimensionalArray(cube.U, dup.U)
+	deepCopyTwoDimensionalArray(cube.L, dup.L)
+	deepCopyTwoDimensionalArray(cube.F, dup.F)
+	deepCopyTwoDimensionalArray(cube.R, dup.R)
+	deepCopyTwoDimensionalArray(cube.B, dup.B)
+	deepCopyTwoDimensionalArray(cube.D, dup.D)
+
+	return dup
+}
+
+func deepCopyTwoDimensionalArray(from [][]int, to [][]int) {
+	for i, v := range from {
+		copy(to[i], v)
+	}
+}
+
+func (cube *Cube) printState() {
+	fmt.Println("       -------                    ")
+	fmt.Printf("       |%d|%d|%d|                    \n", cube.U[0][0], cube.U[0][1], cube.U[0][2])
+	fmt.Printf("       |%d|%d|%d|                    \n", cube.U[1][0], cube.U[1][1], cube.U[1][2])
+	fmt.Printf("       |%d|%d|%d|                    \n", cube.U[2][0], cube.U[2][1], cube.U[2][2])
+
+	fmt.Println(" ------|-----|------------  ")
+
+	fmt.Printf(" |%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|  \n",
+		cube.L[0][0], cube.L[0][1], cube.L[0][2],
+		cube.F[0][0], cube.F[0][1], cube.F[0][2],
+		cube.R[0][0], cube.R[0][1], cube.R[0][2],
+		cube.B[0][0], cube.B[0][1], cube.B[0][2],
+	)
+
+	fmt.Printf(" |%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|  \n",
+		cube.L[1][0], cube.L[1][1], cube.L[1][2],
+		cube.F[1][0], cube.F[1][1], cube.F[1][2],
+		cube.R[1][0], cube.R[1][1], cube.R[1][2],
+		cube.B[1][0], cube.B[1][1], cube.B[1][2],
+	)
+
+	fmt.Printf(" |%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|  \n",
+		cube.L[2][0], cube.L[2][1], cube.L[2][2],
+		cube.F[2][0], cube.F[2][1], cube.F[2][2],
+		cube.R[2][0], cube.R[2][1], cube.R[2][2],
+		cube.B[2][0], cube.B[2][1], cube.B[2][2],
+	)
+
+	fmt.Println(" ------|-----|------------  ")
+
+	fmt.Printf("       |%d|%d|%d|                    \n", cube.D[0][0], cube.D[0][1], cube.D[0][2])
+	fmt.Printf("       |%d|%d|%d|                    \n", cube.D[1][0], cube.D[1][1], cube.D[1][2])
+	fmt.Printf("       |%d|%d|%d|                    \n", cube.D[2][0], cube.D[2][1], cube.D[2][2])
+
+	fmt.Println("       -------                    ")
 }
